6b9217a0fe8e49a4f3ed0ab9b1a7f7dd20e71c75
mkantowski <mkantowski@gmail.com>
2022-06-17 11:01:10 +0200

    aktualziacja

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ Do zrobienia:
   - pip install Flask
 - [ ] Create simple rest api
 - [ ] Pibieranie daty ze strony http://numberapi.com/2/7
+
+### Tutoriale yt:
+- https://www.youtube.com/watch?v=GMppyAPbLYk
--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
+from flask import Flask
+from flask_restful import Resource, Api, abort, reqparse
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+app = Flask(__name__)
+api = Api(app)
 
+class HelloWorld(Resource):
+    def get(self):
+        return {"data": "Hellow World"}
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+api.add_resource(HelloWorld, "/hello")
 
+if __name__ == "__main__":
+    app.run(debug=True)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
